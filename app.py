147b3bbb347397f1ee972755c23ab8ba5ae52af9
Manzo <3mg.manzone@gmail.com>
2026-01-01 16:06:46 +0000

    aggiorno il file python

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def check_answer():
         st.session_state.current_exercise = None 
         st.session_state.input_key += 1 # Questo svuota il campo per il prossimo giro
         # forza refresh
-        st.rerun()
+        #st.rerun()
 
 
 # --- 5° RIGA: Campo di inserimento ---
@@ -184,9 +184,9 @@ st.number_input(
 # 6° Riga: Feedback e attesa
 if st.session_state.feedback:
     st.markdown(st.session_state.feedback, unsafe_allow_html=True)
-    # time.sleep(2.4)
+    time.sleep(2.4)
     st.session_state.feedback = None
-    #st.rerun()
+    st.rerun()
 
 
 # 7° Riga: Report PDF
