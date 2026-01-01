a521c1be67952ce7b5287a88b6535d78834ae2eb
Manzo <3mg.manzone@gmail.com>
2026-01-01 15:00:51 +0000

    aggiorno il file python

--- app.py
+++ app.py
@@ -83,13 +83,16 @@ if st.session_state.history:
 else:
     perc_ok, t_medio, prog = 0, 0, 1
 
-st.markdown(f'<p style="font-size: 28px;
-                        color: #00FF00;
-                        white-space: pre-wrap;
-                        text-align: center;
-                        class="metrics">
-                        ##: {prog} &nbsp; % &nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;ok: {perc_ok:.1f} &nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;T medio: {t_medio:.2f} s</p>', 
-                        unsafe_allow_html=True)
+st.markdown(f"""
+            <p style="font-size: 28px;
+            color: #00FF00;
+            white-space: pre-wrap;
+            text-align: center;
+            class="metrics">
+            ##: {prog} &nbsp; % &nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;ok: {perc_ok:.1f} &nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;T medio: {t_medio:.2f} s
+            </p>
+            """, 
+            unsafe_allow_html=True)
 
 # 4° Riga: Esercizio
 st.markdown(f'<p class="exercise">{st.session_state.current_exercise["q"]}</p>', unsafe_allow_html=True)
